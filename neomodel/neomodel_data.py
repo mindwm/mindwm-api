c26629f712d3e09b0447d65cea72f454b930f715
Your Name <you@example.com>
2024-07-26 11:08:45 +0000

    Added atime field to each data model in neomodel

--- neomodel/neomodel_data.py
+++ neomodel/neomodel_data.py
@@ -19,23 +19,27 @@ config.DRIVER = GraphDatabase.driver(f"bolt://{neo4j_url}")
 class MindwmUser(StructuredNode):
     username = StringProperty(required = True)
     host = RelationshipTo('MindwmHost', 'HAS_MINDWM_HOST')
+    atime = DateTimeProperty(default_now = True)
 
 class MindwmHost(StructuredNode):
     hostname = StringProperty(required = True)
     tmux = RelationshipTo('Tmux', 'HAS_TMUX', cardinality=OneOrMore)
     clipboard = RelationshipTo('ClipBoard', 'HAS_CLIPBOARD', cardinality=OneOrMore)
+    atime = DateTimeProperty(default_now = True)
 
 class Tmux(StructuredNode):
     socket_path = StringProperty(required = True)
     host_id = IntegerProperty(required = True)
     host = RelationshipFrom('MindwmHost', 'HAS_TMUX', cardinality=One)
     session = RelationshipTo('TmuxSession', 'HAS_TMUX_SESSION', cardinality=OneOrMore)
+    atime = DateTimeProperty(default_now = True)
 
 class TmuxSession(StructuredNode):
     name = StringProperty(required = True)
     tmux_id = IntegerProperty(required = True)
     socket_path = RelationshipFrom('Tmux', 'HAS_TMUX', cardinality=One)
     pane = RelationshipTo('TmuxPane', 'HAS_TMUX_PANE', cardinality=OneOrMore)
+    atime = DateTimeProperty(default_now = True)
 
 class TmuxPane(StructuredNode):
     pane_id = IntegerProperty(required = True)
@@ -44,6 +48,7 @@ class TmuxPane(StructuredNode):
     title = StringProperty()
     contextParameters = JSONProperty(default={})
     io_document = Relationship('IoDocument', 'HAS_IO_DOCUMENT')
+    atime = DateTimeProperty(default_now = True)
 
 class IoDocument(StructuredNode):
     uuid = StringProperty(unique_index=True, required = True)
@@ -52,9 +57,11 @@ class IoDocument(StructuredNode):
     ps1 = StringProperty(required = True)
     time = DateTimeProperty(default_now = True)
     tmux_pane = Relationship('TmuxPane', 'HAS_IO_DOCUMENT')
+    atime = DateTimeProperty(default_now = True)
     
 class ClipBoard(StructuredNode):
     uuid = StringProperty(unique_index=True, required = True)
     time = DateTimeProperty(default_now = True)
     data = StringProperty(required = True)
     host = RelationshipFrom('MindwmHost', 'HAS_CLIPBOARD', cardinality=One)
+    atime = DateTimeProperty(default_now = True)
